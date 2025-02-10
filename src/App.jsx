acd205576b982be6c11f8ea21eb7b513ad3e9c61
Bruno <brunoguilher1@gmail.com>
2025-02-10 15:43:01 -0300

    feat: create Tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,60 @@
-import AddTask from './components/AddTask';
-import Task from './components/Tasks';
+import { useState } from "react";
+import AddTask from "./components/AddTask";
+import Task from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description:
+        "Estudar programação para se tornar um desenvolvedor full stack.",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar inglês",
+      description: "Estudar inglês para se tornar fluente.",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar matemática",
+      description:
+        "Estudar matemática para se tornar um desenvolvedor full stack.",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTask = tasks.map((task) => {
+      // PRECISO ATUALIZAR ESSA TAREFA
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      // NÃO PRECISO ATUALIZAR ESSA TARREFA
+      return task;
+    });
+    setTasks(newTask);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTask = tasks.filter(task => task.id !== taskId);
+    setTasks(newTask);
+  }
+
   return (
-    <div>
-      <h1>Gerenciador de Tarefas</h1>
-      <AddTask />
-      <Task />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Task tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
