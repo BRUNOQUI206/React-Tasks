4cc29a885a4d0ef98a1ead4a42f1cbdd37ed66aa
Bruno <brunoguilher1@gmail.com>
2025-02-11 14:42:32 -0300

    feat: create Input

---
+++ src/components/Input.jsx
@@ -0,0 +1,9 @@
+function Input(props) {
+  return (
+    <input className="border border-slate-300 outiline-slate-400 px-4 py-2 rounded-md" 
+        {...props}
+    />
+  );
+}
+
+export default Input;
